1619f3c3dd31f77e6dfa2c18cdef01543ed4dd69
lifeoffredy <124620808+lifeoffredy@users.noreply.github.com>
2023-04-12 08:26:32 -0700

    Update Song 2.rb

--- Song 2.rb
+++ Song 2.rb
@@ -1,19 +1,59 @@
-#congralutions-mac miller song 2
+#song 2
+use_bpm 60
+use_synth :piano
 
 transition = "C:/Users/Fredy Mendoza/Documents/Audacity/10 FREE TRANSITION SOUNDS AND EFFECTS [NO COPYRIGHT].wav"
 
-notes = [:a4,:b4,:c4,:d4,:e4,:f4]
+notes = [:fs4,:fs4,:gs4,:as5]
+st = [3,0.3,0.3,0.3]
 i = 1
 j = 0.25
+i2 = 1
 
 sample transition
 sleep 0.5
 
 sleep 10
 
-6.times do
-  play (notes[i])
-  i = i + 1
-  sleep 0.25
-  amp 0.5
+live_loop :left do
+  4.times do
+    play (notes[i])
+    i = i + 1
+    sleep (st[i2])
+  end
+  #measure 2
+  play :fs4
+  sleep 1
+  play :b3
+  sleep 2
+  play :fs4
+  sleep 1/3.0
+  play :g4
+  sleep 1/3.0
+  play :a4
+  sleep 1/3.0
+  #measure 3
+  play :b4
+  sleep 0.1
+  play :cs4
+  sleep 1.5
+  play :cs4
+  sleep 1/3.0
+  play :d4
+  sleep 1/3.0
+  play :e4
+  sleep 1/3.0
+  #measure 4
+  play :cs5
+  sleep 1
+  play :fs4
+  sleep 2
+  play :b5
+  sleep 0.75
+  play :a5
+  sleep 1/3.0
+  play :g4
+  sleep 1/3.0
+  play :e4
+  sleep 1/3.0
 end
